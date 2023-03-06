8ab2c85cd16727f25051d9fffa9afa146fc44a19
mercymugambi <mercymugambi2016@gmail.com>
2023-03-07 01:13:57 +0300

    Manipulate DOM to open hamburger on click in js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.menu-mobile');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.a-links').forEach((n) => n
+    .addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navMenu.classList.remove('active');
+    }));
