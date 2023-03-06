768b4bd13e8cb08a12e1b8f3d3301928e5dbb1f4
mercymugambi <mercymugambi2016@gmail.com>
2023-03-07 01:47:14 +0300

    Fix eslinter errors

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.menu-mobile');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
 document.querySelectorAll('.a-links').forEach((n) => n
-    .addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    }));
+  .addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  }));
