a7966ebd848e32147aab87a75fd425671c0c54b7
mercymugambi <mercymugambi2016@gmail.com>
2023-03-09 17:26:13 +0300

    Add desktop cards dynamically

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ document.querySelectorAll('.a-links').forEach((n) => n
     navMenu.classList.remove('active');
   }));
 
-//Create the object with elements
-let projectCardArray = [
+//Create the mobile object with elements
+let mobileCardArray = [
     { //card 1 details
         cardTitle: 'TONIC',
         description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
@@ -63,87 +63,159 @@ let projectCardArray = [
         year: '2015',
     },
     ];
+    
+    //desktop objects differs from mobile
+    let desktopCardArray = [
+        { //card 1 details
+            cardTitle: 'TONIC',
+            description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+            company: 'CANOPY',
+            cardImage: '<img class="card-image" src="./assets/images/desktop1.png" alt="project1.png"/>',
+            counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            role: 'Back End Dev',
+            skills: ['HTML', 'CSS', 'Javascript', 'osugo'],
+            counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            year: '2015',
+        },
+        //card2 details
+        {
+            cardTitle: 'Multi-Post Stories',
+            description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+            company: 'FACEBOOK',
+            cardImage: '<img class="card-image" src="./assets/images/desktop2.png" alt="project1.png"/>',
+            counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            role: 'Full Stack Dev',
+            skills: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
+            counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            year: '2015',
+        },
+        //card3 Details
+        {
+            cardTitle: 'Facebook 360',
+            description: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.' ,
+            company: 'FACEBOOK',
+            cardImage: '<img class="card-image" src="./assets/images/desktop3.png" alt="project1.png"/>',
+            counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            role: 'Full Stack Dev',
+            skills: ['HTML', 'CSS', 'Ruby on rails', 'Javascript'],
+            counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            year: '2015',
+        },
+        //card4 Details
+        {
+            cardTitle: 'Uber Navigation',
+            description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+            company: 'Uber',
+            cardImage: '<img class="card-image" src="./assets/images/desktop4.png" alt="project1.png"/>',
+            counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            role: 'Back End Dev',
+            skills: ['HTML', 'CSS', 'Ruby on rails', 'Javascript'],
+            counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
+            year: '2015',
+        },
+        ];
+
  window.onload = function loader() {
 
-//   Cards section
-let body = document.querySelector('body');
-
+// Cards section for the mobile differs from desktop
 //query card section element to hold cards
-const sectionContainer = document.querySelector('.projects-section');
+const mobileCardContainer = document.querySelector('.projects-section');
+function addMobileCard() {
+    let mCard = '';
+    mobileCardArray.forEach((cards, index) => {
+    let skills = '';
+    for (let i = 0; i < cards.skills.length; i++) {
+        skills += `<li>${cards.skills[i]}</li>`; 
+    }
+        mCard += `<div class="card" >
+        ${cards.cardImage}
+        <div class="tonic-area">
+          <h2 class="cards-details">${cards.cardTitle}</h2>
+          <div class="highlights">
+            <span class="highlight-text">${cards.company}</span>
+            ${cards.counter1}
+            <span class="countertext">${cards.role}</span>
+            ${cards.counter2}
+            <span class="year">${cards.year}</span>
+          </div>
+          <article class="cards-description">
+            <p>
+            ${cards.description}
+            </p>
+          </article>
+          <ul class="skills">
+          ${skills}
+          </ul>
+          <button id="popup" type="button">See Project</button>
+        </div>
+      </div>`
+    });
+    mobileCardContainer.innerHTML = mCard;
+  }
 
-projectCardArray.forEach(cards => {
-//div
-const cardDiv = document.createElement('div');
-//add card class to the div class
-cardDiv.classList.add('card');
-//add img to card div first
-cardDiv.innerHTML = `${cards.cardImage}`;
+  //starts of the desktop cards
+const desktopContainer = document.querySelector('.projects-section-desktop');
 
-//add div to webpage
-sectionContainer.append(cardDiv);
-//create tonic area section
-const tonicDiv = document.createElement('div');
-tonicDiv.classList.add('tonic-area');
+function addDesktopCard() {
+    let dCard = '';
+    desktopCardArray.forEach((cards, index) => {
+    let skills = '';
+    for (let i = 0; i < cards.skills.length; i++) {
+        skills += `<li>${cards.skills[i]}</li>`; 
+    }
 
-//Add the text element h2 tonic area
-const h2CardDetails = document.createElement('h2');
-h2CardDetails.textContent = `${cards.cardTitle}`;
-tonicDiv.appendChild(h2CardDetails);
-h2CardDetails.classList.add('cards-details');
-//append h2 tonic to card div
-cardDiv.append(tonicDiv);
-//create div highlights
-const highlightsDiv = document.createElement('div');
-highlightsDiv.classList.add('highlights');
-tonicDiv.append(highlightsDiv);
-//Add elements to this div
-const spanHighlights = document.createElement('span');
-spanHighlights.textContent = `${cards.company}`;
-spanHighlights.classList.add('highlight-text');
-highlightsDiv.appendChild(spanHighlights);
+    if (index === 1 || index ==3){
+        dCard += `<div class="card" id="project2">
+      ${cards.cardImage}
+      <div class="tonic-area">
+        <h2 class="cards-details">${cards.cardTitle}</h2>
+        <div class="highlights">
+          <span class="highlight-text">${cards.company}</span>
+          ${cards.counter1}
+          <span class="countertext">${cards.role}</span>
+          ${cards.counter2}
+          <span class="year">${cards.year}</span>
+        </div>
+        <article class="cards-description">
+          <p>
+          ${cards.description}
+          </p>
+        </article>
+        <ul class="skills">
+        ${skills}
+        </ul>
+        <button id="popup" type="button">See Project</button>
+      </div>
+    </div>`;
+    } else{
 
-//add image adjacent to canopy
-highlightsDiv.insertAdjacentHTML('beforeend', `${cards.counter1}`)
-
-//create spanCountertext
-const spanCountertext = document.createElement('span');
-spanCountertext.classList.add('countertext');
-spanCountertext.textContent = "Back End Dev";
-highlightsDiv.appendChild(spanCountertext);
-
-//add image adjacent to canopy
-highlightsDiv.insertAdjacentHTML('beforeend', `${cards.counter2}`);
-
-//create span year
-const spanYear = document.createElement('span');
-spanYear.classList.add('year');
-spanYear.textContent = `${cards.year}`;
-highlightsDiv.appendChild(spanYear);
-
-//create article section
-const cardsDescription = document.createElement('article');
-cardsDescription.classList.add('cards-description');
-tonicDiv.append(cardsDescription);
-
-//create p on article
-const pDescription = document.createElement('p');
-pDescription.textContent = `${cards.description}`;
-cardsDescription.appendChild(pDescription);
-
-//Add skills ul
-const skillsTec = document.createElement('ul');
-skillsTec.classList.add('skills');
-tonicDiv.append(skillsTec);
-
-//skills li
-for (let i = 0; i < cards.skills.length; i++) {
-    const skillsTecLi = document.createElement('li');
-    skillsTecLi.innerHTML = `${cards.skills[i]}`;
-    skillsTec.append(skillsTecLi);
-}
-
-
-
-
-})
+      dCard += `<div class="card">
+      ${cards.cardImage}
+      <div class="tonic-area">
+        <h2 class="cards-details">${cards.cardTitle}</h2>
+        <div class="highlights">
+          <span class="highlight-text">${cards.company}</span>
+          ${cards.counter1}
+          <span class="countertext">${cards.role}</span>
+          ${cards.counter2}
+          <span class="year">${cards.year}</span>
+        </div>
+        <article class="cards-description">
+          <p>
+          ${cards.description}
+          </p>
+        </article>
+        <ul class="skills">
+        ${skills}
+        </ul>
+        <button id="popup" type="button">See Project</button>
+      </div>
+    </div>`;  
+    }
+    });
+    desktopContainer.innerHTML = dCard;
+  }
+  //call function
+  addMobileCard();
+  addDesktopCard();
 };
