b7b28bee34ff02895a0791961f077488a21c966d
mercymugambi <mercymugambi2016@gmail.com>
2023-03-11 11:11:28 +0300

    Add all object in one place/remove dead links from css

--- js/main.js
+++ js/main.js
@@ -14,13 +14,20 @@ document.querySelectorAll('.a-links').forEach((n) => n
   }));
 
 // Create the mobile object with elements
-const mobileCardArray = [
+const project = [
   { // card 1 details
     id: '1',
     cardTitle: 'TONIC',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     company: 'CANOPY',
+    cardTitle2: 'TONIC',
+    description2: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    company2: 'CANOPY',
+    no: 'd1',
+    cardImage2: './assets/images/desktop1.png" alt="project1.png',
+    role2: 'Back End Dev',
     cardImage: '<img class="card-image" src="./assets/images/project1.png" alt="project1.png"/>',
+    skills2: ['HTML', 'CSS', 'Javascript'],
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
     skills: ['HTML', 'CSS', 'Javascript'],
@@ -33,6 +40,13 @@ const mobileCardArray = [
     cardTitle: 'TONIC',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     company: 'CANOPY',
+    cardTitle2: 'Multi-Post Stories',
+    description2: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    company2: 'FACEBOOK',
+    no: 'd2',
+    cardImage2: './assets/images/desktop2.png" alt="project1.png',
+    role2: 'Full Stack Dev',
+    skills2: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     cardImage: '<img class="card-image" src="./assets/images/project2.png" alt="project1.png" />',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
@@ -46,6 +60,13 @@ const mobileCardArray = [
     cardTitle: 'TONIC',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     company: 'CANOPY',
+    cardTitle2: 'Facebook 360',
+    description2: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    company2: 'FACEBOOK',
+    no: 'd3',
+    cardImage2: './assets/images/desktop3.png" alt="project1.png',
+    role2: 'Full Stack Dev',
+    skills2: ['HTML', 'CSS', 'Ruby on rails', 'Javascript'],
     cardImage: '<img class="card-image" src="./assets/images/project3.png" alt="project1.png" />',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
@@ -59,6 +80,13 @@ const mobileCardArray = [
     cardTitle: 'TONIC',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     company: 'CANOPY',
+    cardTitle2: 'Uber Navigation',
+    description2: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    company2: 'Uber',
+    no: 'd4',
+    cardImage2: './assets/images/desktop4.png" alt="project1.png',
+    role2: 'Back End Dev',
+    skills2: ['HTML', 'CSS', 'Ruby on rails', 'Javascript'],
     cardImage: '<img class="card-image" src="./assets/images/project4.png" alt="project4.png" />',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
@@ -69,11 +97,18 @@ const mobileCardArray = [
 ];
 
 // Create the mobile modal object with elements
-const mobileModalArray = [
+const modals = [
   { // card 1 details
     cardTitle: 'TONIC',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     company: 'CANOPY',
+    cardTitle2: 'Tonic',
+    no: 'd1',
+    description2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
+    company2: 'CANOPY',
+    cardImage2: './assets/images/desktop1.png" alt="project1.png"',
+    role2: 'Front End Dev',
+    skills2: ['HTML', 'CSS', 'Javascript'],
     cardImage: './assets/images/project1.png" alt="project1.png"',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
@@ -89,6 +124,13 @@ const mobileModalArray = [
     cardTitle: 'TONIC',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     company: 'CANOPY',
+    cardTitle2: 'Multi Post stories',
+    no: 'd2',
+    description2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
+    company2: 'Facebook',
+    cardImage2: './assets/images/desktop2.png" alt="project2.png"',
+    role2: 'Back End Dev',
+    skills2: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     cardImage: './assets/images/project2.png" alt="project2.png"',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
@@ -104,6 +146,13 @@ const mobileModalArray = [
     cardTitle: 'TONIC',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     company: 'CANOPY',
+    cardTitle2: 'Facebook 360',
+    no: 'd3',
+    description2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
+    company2: 'facebook',
+    cardImage2: './assets/images/desktop3.png" alt="project3.png"',
+    role2: 'Back End Dev',
+    skills2: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     cardImage: './assets/images/project3.png" alt="project3.png"',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
@@ -117,133 +166,20 @@ const mobileModalArray = [
   // card4 Details
   {
     cardTitle: 'TONIC',
-    closeBtn: '&times;',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     company: 'CANOPY',
+    cardTitle2: 'Uber Navigation',
+    no: 'd4',
+    description2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
+    company2: 'Uber',
+    cardImage2: './assets/images/desktop4.png" alt="project4.png"',
+    role2: 'Back End Dev',
+    skills2: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     cardImage: './assets/images/project4.png" alt="project4.png"',
     counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     role: 'Back End Dev',
     id: '4',
     skills: ['HTML', 'CSS', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-    sourceLink: 'https://github.com/mercymugambi/portfolio',
-    demoLink: 'https://mercymugambi.github.io/portfolio/',
-  },
-];
-
-// desktop objects differs from mobile
-const desktopCardArray = [
-  { // card 1 details
-    cardTitle: 'TONIC',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    company: 'CANOPY',
-    id: 'd1',
-    cardImage: '<img class="card-image" src="./assets/images/desktop1.png" alt="project1.png"/>',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Back End Dev',
-    skills: ['HTML', 'CSS', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-  },
-  // card2 details
-  {
-    cardTitle: 'Multi-Post Stories',
-    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    company: 'FACEBOOK',
-    id: 'd2',
-    cardImage: '<img class="card-image" src="./assets/images/desktop2.png" alt="project1.png"/>',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Full Stack Dev',
-    skills: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-  },
-  // card3 Details
-  {
-    cardTitle: 'Facebook 360',
-    description: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    company: 'FACEBOOK',
-    id: 'd3',
-    cardImage: '<img class="card-image" src="./assets/images/desktop3.png" alt="project1.png"/>',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Full Stack Dev',
-    skills: ['HTML', 'CSS', 'Ruby on rails', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-  },
-  // card4 Details
-  {
-    cardTitle: 'Uber Navigation',
-    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
-    company: 'Uber',
-    id: 'd4',
-    cardImage: '<img class="card-image" src="./assets/images/desktop4.png" alt="project1.png"/>',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Back End Dev',
-    skills: ['HTML', 'CSS', 'Ruby on rails', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-  },
-];
-
-// Create the desktop modal object with elements
-const desktopModalArray = [
-  { // card 1 details
-    cardTitle: 'Tonic',
-    id: 'd1',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
-    company: 'CANOPY',
-    cardImage: './assets/images/desktop1.png" alt="project1.png"',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Front End Dev',
-    skills: ['HTML', 'CSS', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-    sourceLink: 'https://github.com/mercy/portfolio',
-    demoLink: 'https://mercymugambi.github.io/portfolio/',
-  },
-  // card2 details
-  {
-    cardTitle: 'Multi Post stories',
-    id: 'd2',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
-    company: 'Facebook',
-    cardImage: './assets/images/desktop2.png" alt="project2.png"',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Back End Dev',
-    skills: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-    sourceLink: 'https://github.com/mercymugambi/portfolio',
-    demoLink: 'https://mercymugambi.github.io/portfolio/',
-  },
-  // card3 Details
-  {
-    cardTitle: 'Facebook 360',
-    id: 'd3',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
-    company: 'facebook',
-    cardImage: './assets/images/desktop3.png" alt="project3.png"',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Back End Dev',
-    skills: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
-    counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    year: '2015',
-    sourceLink: 'https://github.com/mercymugambi/portfolio',
-    demoLink: 'https://mercymugambi.github.io/portfolio/',
-  },
-  // card4 Details
-  {
-    cardTitle: 'Uber Navigation',
-    id: 'd4',
-    closeBtn: '&times;',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. printer took a galley of type and scrambled it 1960s with the relea',
-    company: 'Uber',
-    cardImage: './assets/images/desktop4.png" alt="project4.png"',
-    counter1: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
-    role: 'Back End Dev',
-    skills: ['HTML', 'Ruby on rails', 'CSS', 'Javascript'],
     counter2: '<img src="./assets/images/Counter.png" alt="Counter.png" />',
     year: '2015',
     sourceLink: 'https://github.com/mercymugambi/portfolio',
@@ -257,7 +193,7 @@ window.onload = function loader() {
   const mobileCardContainer = document.querySelector('.projects-section');
   function addMobileCard() {
     let mCard = '';
-    mobileCardArray.forEach((cards) => {
+    project.forEach((cards) => {
       let skills = '';
       for (let i = 0; i < cards.skills.length; i += 1) {
         skills += `<li>${cards.skills[i]}</li>`;
@@ -295,56 +231,56 @@ window.onload = function loader() {
 
   function addDesktopCard() {
     let dCard = '';
-    desktopCardArray.forEach((cards, index) => {
-      let skills = '';
-      for (let i = 0; i < cards.skills.length; i += 1) {
-        skills += `<li>${cards.skills[i]}</li>`;
+    project.forEach((cards, index) => {
+      let skills2 = '';
+      for (let i = 0; i < cards.skills2.length; i += 1) {
+        skills2 += `<li>${cards.skills2[i]}</li>`;
       }
 
       if (index === 1 || index === 3) {
         dCard += `<div class="card project2">
-      ${cards.cardImage}
+        <img class="card-image" src="${cards.cardImage2}" alt="project1.png"/>'
       <div class="tonic-area">
-        <h2 class="cards-details">${cards.cardTitle}</h2>
+        <h2 class="cards-details">${cards.cardTitle2}</h2>
         <div class="highlights">
-          <span class="highlight-text">${cards.company}</span>
+          <span class="highlight-text">${cards.company2}</span>
           ${cards.counter1}
-          <span class="countertext">${cards.role}</span>
+          <span class="countertext">${cards.role2}</span>
           ${cards.counter2}
           <span class="year">${cards.year}</span>
         </div>
         <article class="card-description">
           <p>
-          ${cards.description}
+          ${cards.description2}
           </p>
         </article>
         <ul class="skills">
-        ${skills}
+        ${skills2}
         </ul>
-        <a href ="#${cards.id}" class="popup">See Project</a>
+        <a href ="#${cards.no}" class="popup">See Project</a>
       </div>
     </div>`;
       } else {
         dCard += `<div class="card">
-      ${cards.cardImage}
+        <img class="card-image" src="${cards.cardImage2}" alt="project1.png"/>'
       <div class="tonic-area">
-        <h2 class="cards-details">${cards.cardTitle}</h2>
+        <h2 class="cards-details">${cards.cardTitle2}</h2>
         <div class="highlights">
-          <span class="highlight-text">${cards.company}</span>
+          <span class="highlight-text">${cards.company2}</span>
           ${cards.counter1}
-          <span class="countertext">${cards.role}</span>
+          <span class="countertext">${cards.role2}</span>
           ${cards.counter2}
           <span class="year">${cards.year}</span>
         </div>
         <article class="card-description">
           <p>
-          ${cards.description}
+          ${cards.description2}
           </p>
         </article>
         <ul class="skills">
-        ${skills}
+        ${skills2}
         </ul>
-        <a href="#${cards.id}" class="popup">See Project</a>
+        <a href="#${cards.no}" class="popup">See Project</a>
       </div>
     </div>`;
       }
@@ -355,7 +291,7 @@ window.onload = function loader() {
   const mobileModalContainer = document.querySelector('.mobile-popup-window');
   function addMobileModal() {
     let modalCard = '';
-    mobileModalArray.forEach((cards, index) => {
+    modals.forEach((cards, index) => {
       let skills = '';
       for (let i = 0; i < cards.skills.length; i += 1) {
         skills += `<li>${cards.skills[i]}</li>`;
@@ -398,35 +334,35 @@ window.onload = function loader() {
   const desktopModalContainer = document.querySelector('.desktop-popup-window');
   function addDesktopModal() {
     let desktopModalCard = '';
-    desktopModalArray.forEach((cards) => {
-      let skills = '';
-      for (let i = 0; i < cards.skills.length; i += 1) {
-        skills += `<li>${cards.skills[i]}</li>`;
+    modals.forEach((cards) => {
+      let skills2 = '';
+      for (let i = 0; i < cards.skills2.length; i += 1) {
+        skills2 += `<li>${cards.skills2[i]}</li>`;
       }
-      desktopModalCard += `<div class="desktopModal" id ="${cards.id}">
+      desktopModalCard += `<div class="desktopModal" id ="${cards.no}">
           <div class="card">
           <a href="#" class="close-btn">&times;</a>
           <div class="tonic-area">
-              <h2 class="cards-details">${cards.cardTitle}</h2>
+              <h2 class="cards-details">${cards.cardTitle2}</h2>
             <div class="highlights">
-              <span class="highlight-text">${cards.company}</span>
+              <span class="highlight-text">${cards.company2}</span>
               <img src="./assets/images/Counter.png" alt="Counter.png" />
-              <span class="countertext">${cards.role}</span>
+              <span class="countertext">${cards.role2}</span>
               <img src="./assets/images/Counter.png" alt="Counter.png" />
               <span class="year">${cards.year}</span>
             </div>
           </div>
-          <img class="card-image" src="${cards.cardImage}" alt="project1.png"/>
+          <img class="card-image" src="${cards.cardImage2}" alt="project1.png"/>
           <div class="tonic-area2">
             
             <article class="cards-description">
               <p>
-              ${cards.description}
+              ${cards.description2}
                  </p>
             </article>
             <div>
             <ul class="skills">
-            ${skills}
+            ${skills2}
             </ul>
             <div class="border"></div>
               <div class="modal-links">
